02abafb81e35c15eafdb444c21768a2f3d361824
OscarT231 <otunjanor@ucentral.edu.co>
2025-12-02 01:42:41 -0500

    webScraping en init

--- Helpers/__init__.py
+++ Helpers/__init__.py
@@ -1,4 +1,5 @@
 from .mongoDB import MongoDB
 from .funciones import Funciones
 from .elastic import ElasticSearch
+from .webScraping import WebScraping
 __all__ = ['MongoDB', 'Funciones', 'ElasticSearch', 'WebScraping']
